11f32f3f8c26aca311f3e8c8f28fe21f47142802
Parvej Khan <1999monustp@gmail.com>
2025-01-30 12:18:25 +0530

    updated the vercel.json routes

--- git.py
+++ git.py
@@ -1,10 +1,11 @@
 import os
 
 # commitMsg = input("Enter Commit Message: ")
-commitMsg = '"updated the vercel.json"'
+commitMsg = '''"updated the vercel.json routes"'''
 print("git add . running")
 os.system("git add .")
-print("git commit -m {0} ".format(commitMsg))
+
+print("-----------\ngit commit -m {0} -----------\n".format(commitMsg))
 os.system("git commit -m {0} ".format(commitMsg))
 print("pushing")
 os.system("git push origin main")
